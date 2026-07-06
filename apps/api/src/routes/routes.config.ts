f5abbb0d2b455617db2c25dd6bb67d51c74610c8
Surajshegukar <surajshegukar2732@gmail.com>
2026-07-06 16:33:49 +0530

    modified:   apps/api/src/routes/routes.config.ts

--- apps/api/src/routes/routes.config.ts
+++ apps/api/src/routes/routes.config.ts
@@ -9,6 +9,26 @@ export function registerRoutes(app: Application): void {
   app.use("/api/conversations", conversationRoutes);
 
   app.get("/health", (_req: Request, res: Response) => {
-    res.json({ status: "OK", timestamp: new Date().toISOString() });
+    res.json({ status: "OK", timestamp: new Date().toISOString(),
+      // all env variables 
+      env:{
+          Node_ENV: process.env.NODE_ENV,
+          PORT: process.env.PORT,
+          MONGO_URI: process.env.MONGO_URI,
+          CLIENT_URL: process.env.CLIENT_URL,
+          CORS_ORIGIN: process.env.CORS_ORIGIN,
+          RESEND_API_KEY: process.env.RESEND_API_KEY,
+          EMAIL_FROM: process.env.EMAIL_FROM,
+          GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+          GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+          GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+          GITHUB_CLIENT_SECRET_KEY: process.env.GITHUB_CLIENT_SECRET,
+          AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+          AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+          AWS_REGION: process.env.AWS_REGION,
+          AWS_S3_BUCKET: process.env.AWS_S3_BUCKET,
+      }
+
+     });
   });
 }
